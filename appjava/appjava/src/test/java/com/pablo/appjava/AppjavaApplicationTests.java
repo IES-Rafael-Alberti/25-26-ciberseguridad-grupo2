78b25a12bb7901afc78d501aacd3bbb370b35642
PabloGonz <pablogonzalezsilva6@gmail.com>
2025-10-09 14:07:44 +0200

    Añadido proyecto appjava con Dockerfile y Spring Boot

---
+++ appjava/appjava/src/test/java/com/pablo/appjava/AppjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.appjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ appjava/src/main/java/com/pablo/appjava/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.pablo.appjava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hola desde Java en Docker";
+    }
+}
