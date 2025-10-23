321ca773cc285894e50d5eec7790728232f3cc3a
lromnav497 <lromnav497@gmail.com>
2025-10-23 10:52:33 +0200

    API REST nodejs añadida

--- PPS/Infraestructura-Desarrollo/nodejs/src/index.js
+++ PPS/Infraestructura-Desarrollo/nodejs/src/index.js
@@ -1,11 +1,22 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+import express from "express";
+import cors from "cors";
+import sequelize from "./db/db.js";
+import usersRouter from "./routes/users.js";
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/users", usersRouter);
 app.get('/', (req, res) => {
   res.send('Hola mundo desde Node.js!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+// Sync database
+sequelize.sync({ alter: true })
+  .then(() => console.log("Database synchronized"))
+  .catch(err => console.error("Error synchronizing the database:", err));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
